d5076cd391b3c8d3377bb7cfb35a9cfd465afec5
rudraksh97 <rudrakshiitd@outlook.com>
2022-07-31 17:57:32 +0530

    1480

---
+++ array/FourteenEighty.java
@@ -0,0 +1,20 @@
+class FourteenEighty {
+    public int[] runningSum(int[] nums) {
+        if (nums.length == 0) {
+            return nums;
+        }
+
+        int current_sum = 0;
+        int index = 0;
+        int[] output = new int[nums.length];
+
+        while(index < nums.length) {
+            int value = nums[index];
+            current_sum += value;
+            output[index] = current_sum;
+            index += 1;
+        }
+
+        return output;
+    }
+}
